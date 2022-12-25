ce3655edf483411d7b889dd6d42c9d045ef3d3b5
Keith Ma <keithchezzy@gmail.com>
2022-12-25 10:44:12 -0800

    commit tree 1d5437142f9a1accf9a7425f8473afaa4953eaf3

--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,3 +1,10 @@
 class BakedGood < ActiveRecord::Base
+  belongs_to :bakery
   # add association macro here
+  def self.by_price 
+    all.order(price: :desc)
+  end
+  def self.most_expensive
+    self.all.order(price: :desc).first
+  end
 end
